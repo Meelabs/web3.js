d5fc88133d853094e6930e81136c400eb523916a
Tomasz <t.fidecki@gmail.com>
2025-03-10 12:20:39 +0100

    Expose utils from Web3 class as static property (#1)
    Co-authored-by: Tomasz Fidecki <tomasz.fidecki@meelogic.com>

--- packages/web3/types/index.d.ts
+++ packages/web3/types/index.d.ts
@@ -37,7 +37,7 @@ export default class Web3 extends AbstractWeb3Module {
     static modules: Modules;
     static readonly givenProvider: provider | null;
 
-    utils: Utils;
+    static readonly utils: Utils;
     eth: Eth;
     shh: Shh;
     bzz: Bzz;
